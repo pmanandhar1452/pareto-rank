32127fec132626e3a87e2f8d3997fcfd6b8fd151
Prakash Manandhar <jsaaymi@gmail.com>
2020-07-25 15:38:49 -0400

    Adding simple test data

---
+++ pareto_rank_tests/test_data_2d_minmin000gen.py
@@ -0,0 +1,45 @@
+'''
+MIT License
+
+Copyright (c) 2020 Prakash Manandhar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+''' Generates test cases '''
+
+import math
+import random
+
+def boundary_fn (x, y):
+    if y < 1.0/math.sinh(x):
+        rv = False
+    else:
+        rv = True
+    return rv
+
+with open('test_data_2d_minmin000.csv', 'w') as fp:
+    fp.write('id, v0, v1, manual_rank\n')
+    for i in range(20):
+        valid = False
+        while not valid:
+            x = random.uniform(0.1, 10)
+            y = random.uniform(0.1, 10)
+            valid = boundary_fn(x, y)
+        fp.write(f'{i}, {x}, {y}, \n')
